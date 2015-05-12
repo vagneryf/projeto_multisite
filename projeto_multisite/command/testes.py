28f11f7627bfa3b7f0ccdd5439db7d44a1b26366
Vagner <vagner.yf@gmail.com>
2015-05-12 18:01:57 -0300

    Teste

--- projeto_multisite/command/testes.py
+++ projeto_multisite/command/testes.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+# Para rodar o comando colocar as configurações abaixo
+import os, sys #, re, datetime
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# conf_location = 'settings.local' if sys.path[0][0:2].upper() == 'D:' else 'settings.remote'
+conf_location = 'settings' if sys.path[0][0:2].upper() == 'D:'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", conf_location)
+
+import django
+django.setup()
+
 from django.contrib.auth.models import User
 from profiles.models import Profile
 from artigos.models import Artigo, Categoria
